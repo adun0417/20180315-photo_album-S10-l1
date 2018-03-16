fd241de3db9837d2a516bd3fdcfe44bb8af97a1a
CHUANG TZU HSUAN <varok1992417@gmail.com>
2018-03-16 19:20:10 +0800

    photo_album-S10-done!

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,50 +1,44 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, :only => [:show, :edit, :update, :destroy]
+  
   def index
-    @photos =Photo.all
+    @photos = Photo.all
   end
 
   def new
-    @photo =Photo.new
+    @photo = Photo.new
   end
 
   def create
     @photo = Photo.new(photo_params)
-    if
-      @photo.save
+    if @photo.save
       redirect_to photos_url
     else
-      render :action => :new
+      render :new
     end
   end
 
-  def show
-    @photo = Photo.find(params[:id])
-  end
-
-  def edit
-    @photo = Photo.find(params[:id])
-  end
-
   def update
-    if
-      @photo = Photo.find(params[:id])
-      @photo.update_attributes(photo_params)
-    redirect_to photo_path(@photo)
+    if @photo.update_attributes(photo_params)
+      redirect_to photo_path(@photo)
     else
-      render :action => :edit
+      render :edit
     end
   end
 
   def destroy
-    @photo = Photo.find(params[:id])
     @photo.destroy
+
     redirect_to photos_url
   end
 
   private
 
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
+
   def photo_params
     params.require(:photo).permit(:title, :date, :description, :file_location)
   end
-
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ApplicationRecord
+  mount_uploader :file_location, PhotoImageUploader
   validates_presence_of :title, :date, :description, :file_location
+  validates_numericality_of :date
 end
